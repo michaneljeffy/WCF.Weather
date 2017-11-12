3589b35bbf5402e284304ac1f645c91ca1757e30
michaneljeffy <845072805@qq.com>
2017-11-12 23:04:04 +0800

    create project

---
+++ WeatherService.DAL/SqlHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace WeatherService.DAL
+{
+    public static class SqlHelper
+    {
+        private static string connectString = ConfigurationManager.ConnectionStrings["MySqlConnect"].ConnectionString;
+        public static T ExecScalarSql<T>(string sqlContent)
+        {
+            try
+            {
+                //var connectString = ConfigurationManager.ConnectionStrings["MySqlConnect"].ConnectionString;
+                using (var connect = new MySqlConnection(connectString))
+                {
+                    var resutl = connect.ExecuteScalar<T>(sqlContent,CommandType.Text);
+                    connect.Close();
+                    return resutl;
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static int ExecuteNonQuery(string sqlContent)
+        {
+            try
+            {
+                using (var connect = new MySqlConnection(connectString))
+                {
+                    var result = connect.Execute(sqlContent, CommandType.Text);
+                    return result;
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+           
+         }
+
+        public static IDbConnection OpenConnect()
+        {           
+            using (var connect = new MySqlConnection(connectString))
+            {
+                connect.Open();
+                return connect;
+            }
+        }
+    }
+}
---
+++ WeatherService.DAL/UserDB.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using WeatherService.Model;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace WeatherService.DAL
+{
+    public class UserDB
+    {
+        public User GetAll()
+        {
+            string query = "select * from users";
+            User result = SqlHelper.ExecScalarSql<User>(query);
+            return result;
+        }
+
+        public User GetOne(int id)
+        {
+            using (IDbConnection connection = SqlHelper.OpenConnect())
+            {
+                string query = "select * from user where Id=@id";
+                return connection.Query<User>(query, new { id = id })
+                                .SingleOrDefault<User>();
+            }
+
+        }
+
+        public bool Add(User user)
+        {
+            string query = string.Format("INSERT INTO [dbo].[User]([UserName],[Password],[Discribe],[SubmitTime]) VALUES('{0}','{1}','{2}','{3}')",
+                user.UserName, user.Password, user.Discribe, user.SubmitTime);
+            int result = SqlHelper.ExecuteNonQuery(query);
+            if (result > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Save(Model.User user)
+        {
+            string query = string.Format("UPDATE [dbo].[User] SET [UserName] = '{0}',[Discribe] = '{2}',[SubmitTime] = '{3}' WHERE UserID = {4}", user.UserName, user.Password, user.Discribe, user.SubmitTime, user.UserID);
+            int result = SqlHelper.ExecuteNonQuery(query);
+            if (result > 0)
+                return true;
+            else
+                return false;
+        }
+
+        //删除
+        public bool Remove(int UserID)
+        {
+            string query = string.Format("DELETE FROM [dbo].[User] WHERE UserID = {0}", UserID);
+            int result = SqlHelper.ExecuteNonQuery(query);
+            if (result > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
